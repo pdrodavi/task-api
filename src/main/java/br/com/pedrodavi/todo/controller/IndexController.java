c8e5b8e623d105018442c1e8c564fa07bb373a9a
Pedro Davi <31486060+pdrodavi@users.noreply.github.com>
2021-03-09 00:03:22 -0300

    Update IndexController.java

--- src/main/java/br/com/pedrodavi/todo/controller/IndexController.java
+++ src/main/java/br/com/pedrodavi/todo/controller/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 
     @GetMapping
     public String index() {
-        return "swagger-ui";
+        return "<a href='/swagger-ui.html'>Documentação</a>";
     }
 
 }
